6bf4c1854f6a7004808a80f2dedc59c974a5f738
Marco <cwhong8087@gmail.com>
2023-12-03 14:46:04 +0900

    “updated”

--- src/Average.js
+++ src/Average.js
@@ -1,4 +1,4 @@
-import { useCallback, useMemo, useState } from "react";
+import { useCallback, useMemo, useRef, useState } from "react";
 
 const getAverage = (numbers) => {
   console.log("평균값 계산 중...");
@@ -10,6 +10,7 @@ const getAverage = (numbers) => {
 const Average = () => {
   const [list, setList] = useState([]);
   const [number, setNumber] = useState("");
+  const inputEl = useRef(null);
 
   // const onChange = (e) => {
   //   setNumber(e.target.value);
@@ -20,6 +21,7 @@ const Average = () => {
   //   setNumber("");
   // };
 
+  // useCallback 이용
   const onChange = useCallback((e) => {
     setNumber(e.target.value);
   }, []); // 컴포넌트가 처음 렌더링될 때만 함수 생성
@@ -27,13 +29,14 @@ const Average = () => {
     const nextList = list.concat(parseInt(number));
     setList(nextList);
     setNumber("");
+    inputEl.current.focus(); // useRef 이용
   }, [number, list]); // number 혹은 list가 바뀌었을 때만 함수 생성
 
   const avg = useMemo(() => getAverage(list), [list]);
 
   return (
     <div>
-      <input value={number} onChange={onChange} />
+      <input value={number} onChange={onChange} ref={inputEl} />
       <button onClick={onInsert}>등록</button>
       <ul>
         {list.map((value, index) => (
