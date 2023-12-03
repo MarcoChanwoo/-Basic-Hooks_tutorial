9025376a5be858a39f549ab5b1b6c3542063dadc
Marco <cwhong8087@gmail.com>
2023-12-03 14:25:46 +0900

    “updated”

--- src/Average.js
+++ src/Average.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const getAverage = (numbers) => {
   console.log("평균값 계산 중...");
   if (numbers.length === 0) return 0;
@@ -8,4 +10,13 @@ const getAverage = (numbers) => {
 const Average = () => {
   const [list, setList] = useState([]);
   const [number, setNumber] = useState("");
+
+  const onChange = (e) => {
+    setNumber(e.target.value);
+  };
+  const onInsert = (e) => {
+    const nextList = list.concat(parseInt(number));
+    setList(nextList);
+    setNumber("");
+  };
 };
