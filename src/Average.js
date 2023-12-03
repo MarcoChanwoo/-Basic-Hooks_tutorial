5b4e20958f6a472b88c64d1a04cbee26db45b865
Marco <cwhong8087@gmail.com>
2023-12-03 14:24:05 +0900

    “updated”

---
+++ src/Average.js
@@ -0,0 +1,11 @@
+const getAverage = (numbers) => {
+  console.log("평균값 계산 중...");
+  if (numbers.length === 0) return 0;
+  const sum = numbers.reduce((a, b) => a + b);
+  return sum / numbers.length;
+};
+
+const Average = () => {
+  const [list, setList] = useState([]);
+  const [number, setNumber] = useState("");
+};
